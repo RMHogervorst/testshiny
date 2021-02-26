6700442364ce81aee038473d1a935adcf8c50be3
Roel M. Hogervorst <hogervorst.rm@gmail.com>
2021-02-26 16:28:58 +0100

    this is it

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM rocker/shiny:4.0.4
 RUN install2.r rsconnect
 WORKDIR /home/shinyusr
 COPY app.R app.R 
-#COPY deploy.R deploy.R
-CMD Rscript -e 'rsconnect::setAccountInfo(name=${SHINY_ACC_NAME}, token=${TOKEN}, secret=${SECRET}); rsconnect::deployApp(appName = ${MASTER_NAME},appTitle = "shinyapplication")'
+COPY deploy.R deploy.R
+CMD Rscript deploy.R
---
+++ deploy.R
@@ -0,0 +1,23 @@
+library(rsconnect)
+
+# Print a list of app dependencies. Libraries need to be loaded
+# before publishing so deployApp() knows what is necessary.
+error_on_missing_name <- function(name){
+    var <- Sys.getenv(name, unset=NA)
+    if(is.na(var)){
+        stop(paste0("cannot find ",name),call. = FALSE)
+    }
+    gsub("\"", '',var)
+}
+
+# Set the account info for deployment.
+setAccountInfo(name   = error_on_missing_name("SHINY_ACC_NAME"),
+               token  = error_on_missing_name("TOKEN"),
+               secret = error_on_missing_name("SECRET"))
+
+# Deploy the application.
+deployApp(
+    appFiles = c("app.R" #, "renv/activate.R","renv.lock",'.Rprofile'
+                       ),
+    appName = error_on_missing_name("MASTER_NAME"),
+    appTitle = "shinyapplication")
